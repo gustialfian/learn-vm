98fc5a074cc586f095f5d9f661e3a5dcf9140d7b
Gusti Alfian <gusti.alfian@komunal.co.id>
2023-06-28 20:31:15 +0700

    EP02: done

--- src/instructions.js
+++ src/instructions.js
@@ -1,9 +1,9 @@
-const MOV_LIT_R1  = 0x10;
-const MOV_LIT_R2  = 0x11;
-const ADD_REG_REG = 0x12;
 
 module.exports = {
-  MOV_LIT_R1,
-  MOV_LIT_R2,
-  ADD_REG_REG,
+  MOV_LIT_REG: 0x10,
+  MOV_REG_REG: 0x11,
+  MOV_REG_MEM: 0x12,
+  MOV_MEM_REG: 0x13,
+  ADD_REG_REG: 0x14,
+  JMP_NOT_EQ : 0x15,
 };
